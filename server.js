3414a7967a6a2cb5e6e5abd7b8d53ff5a3f79812
Michael Williams <manicdesigner@gmail.com>
2020-03-05 20:05:19 -0500

    built out the test route, 404, and next error.

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const helmet = require('helmet');
-
+const recipesRouter = require('./recipes/recipes-router');
 const router = express();
+
+router.use(helmet());
+router.use(express.json());
+
+router.get('/', (req, res) => {
+  res.send('recipe book is up and running');
+});
+
+router.use((req, res) => {
+  res
+    .status(404)
+    .json({ message: '404 sorry we hit the fail whale, page not found' });
+});
+
+router.use((err, req, res, next) => {
+  res.status(500).json({ message: 'internal server error' });
+});
 
 module.exports = router;
