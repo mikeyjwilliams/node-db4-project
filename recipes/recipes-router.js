8305acc5addc63e659c6fd888eaf4ff01a4ac8e3
Michael Williams <manicdesigner@gmail.com>
2020-03-05 21:48:32 -0500

    built 1st stretch GET route

--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const recipeModel = require('./recipes-model');
+
+router.get('/api/recipes', async (req, res, next) => {
+  try {
+    const recipes = await recipeModel.getRecipes();
+    res.status(200).json(recipes);
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
