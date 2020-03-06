80f1b345276ad927155fd77c140e866091f681d0
Mikey Williams <manicdesigner@gmail.com>
2020-03-06 17:25:27 -0500

    format code and comments added

--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -8,12 +8,12 @@ const recipeModel = require('./recipes-model');
  * @return []
  */
 router.get('/', async (req, res, next) => {
-  try {
-    const recipes = await recipeModel.getRecipes();
-    res.status(200).json(recipes);
-  } catch (err) {
-    next(err);
-  }
+	try {
+		const recipes = await recipeModel.getRecipes();
+		res.status(200).json(recipes);
+	} catch (err) {
+		next(err);
+	}
 });
 
 /**
@@ -22,36 +22,38 @@ router.get('/', async (req, res, next) => {
  * @return []
  */
 router.get('/:id/shoppingList', async (req, res, next) => {
-  const { id } = req.params;
-  try {
-    const shoppingList = await recipeModel.getShoppingList(id);
-    if (shoppingList) {
-      res.status(200).json(shoppingList);
-    } else {
-      res
-        .status(400)
-        .json({ message: 'recipe ID not found for shopping list.' });
-    }
-  } catch (err) {
-    next(err);
-  }
+	const { id } = req.params;
+	try {
+		const shoppingList = await recipeModel.getShoppingList(id);
+		if (shoppingList) {
+			res.status(200).json(shoppingList);
+		} else {
+			res
+				.status(400)
+				.json({ message: 'recipe ID not found for shopping list.' });
+		}
+	} catch (err) {
+		next(err);
+	}
 });
 
 /**
- *
+ * @type: GET
+ * @route /api/recipes/:id/instructions
+ * @return []
  */
 router.get('/:id/instructions', async (req, res, next) => {
-  const { id } = req.params;
-  try {
-    const instructions = await recipeModel.getInstructions(id);
-    if (instructions) {
-      res.status(200).json(instructions);
-    } else {
-      res.status(400).json({ message: 'recipe ID for instructions not found' });
-    }
-  } catch (err) {
-    next(err);
-  }
+	const { id } = req.params;
+	try {
+		const instructions = await recipeModel.getInstructions(id);
+		if (instructions) {
+			res.status(200).json(instructions);
+		} else {
+			res.status(400).json({ message: 'recipe ID for instructions not found' });
+		}
+	} catch (err) {
+		next(err);
+	}
 });
 
 module.exports = router;
