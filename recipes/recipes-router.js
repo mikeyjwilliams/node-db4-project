83d2f8863db387222f0193abafb66113b102a462
Michael Williams <manicdesigner@gmail.com>
2020-03-07 15:34:15 -0500

    stretch goals accomplished same night but after class put marker in to note.

--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -3,39 +3,21 @@ const router = express.Router();
 const recipeModel = require('./recipes-model');
 
 /**
+ *! stretch goals accomplished after class.
+ */
+
+/**
  * @type GET
  * @route /api/recipes
  * @return []
  */
 router.get('/', async (req, res, next) => {
-	try {
-		const recipes = await recipeModel.getRecipes();
-		res.status(200).json(recipes);
-	} catch (err) {
-		next(err);
-	}
-});
-
-/**
- * @type POST
- * @route /api/recipes
- * @return {}
- */
-router.post('/', async (req, res, next) => {
-	const { name } = req.body;
-	if (!name) {
-		return res.status(400).json({ message: 'recipe_name is required' });
-	}
-	const recipe = {
-		name: name
-	};
-	try {
-		const recipePost = await recipeModel.postRecipe(recipe);
-		res.status(201).json(recipePost);
-	} catch (err) {
-		console.log(err);
-		next(err);
-	}
+  try {
+    const recipes = await recipeModel.getRecipes();
+    res.status(200).json(recipes);
+  } catch (err) {
+    next(err);
+  }
 });
 
 /**
@@ -44,19 +26,19 @@ router.post('/', async (req, res, next) => {
  * @return []
  */
 router.get('/:id/shoppingList', async (req, res, next) => {
-	const { id } = req.params;
-	try {
-		const shoppingList = await recipeModel.getShoppingList(id);
-		if (shoppingList) {
-			res.status(200).json(shoppingList);
-		} else {
-			res
-				.status(400)
-				.json({ message: 'recipe ID not found for shopping list.' });
-		}
-	} catch (err) {
-		next(err);
-	}
+  const { id } = req.params;
+  try {
+    const shoppingList = await recipeModel.getShoppingList(id);
+    if (shoppingList) {
+      res.status(200).json(shoppingList);
+    } else {
+      res
+        .status(400)
+        .json({ message: 'recipe ID not found for shopping list.' });
+    }
+  } catch (err) {
+    next(err);
+  }
 });
 
 /**
@@ -65,17 +47,40 @@ router.get('/:id/shoppingList', async (req, res, next) => {
  * @return []
  */
 router.get('/:id/instructions', async (req, res, next) => {
-	const { id } = req.params;
-	try {
-		const instructions = await recipeModel.getInstructions(id);
-		if (instructions) {
-			res.status(200).json(instructions);
-		} else {
-			res.status(400).json({ message: 'recipe ID for instructions not found' });
-		}
-	} catch (err) {
-		next(err);
-	}
+  const { id } = req.params;
+  try {
+    const instructions = await recipeModel.getInstructions(id);
+    if (instructions) {
+      res.status(200).json(instructions);
+    } else {
+      res.status(400).json({ message: 'recipe ID for instructions not found' });
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+/**
+ *! Extra route added *not* in requirements for *MVP*.
+ * @type POST
+ * @route /api/recipes
+ * @return {}
+ */
+router.post('/', async (req, res, next) => {
+  const { name } = req.body;
+  if (!name) {
+    return res.status(400).json({ message: 'recipe_name is required' });
+  }
+  const recipe = {
+    name: name,
+  };
+  try {
+    const recipePost = await recipeModel.postRecipe(recipe);
+    res.status(201).json(recipePost);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
 });
 
 module.exports = router;
